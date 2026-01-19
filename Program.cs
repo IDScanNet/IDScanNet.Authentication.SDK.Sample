469566e403cc02934dc341e594a23f6653bfcd2d
Denis Kudryavtsev <dkudryavtsev@idscan.net>
2026-01-19 13:53:51 +0500

    The test application has been updated.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using IDScanNet.Authentication;
 using IDScanNet.Authentication.SDK;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -13,173 +14,157 @@ namespace TestApp;
 
 public class Program
 {
+    private static string logFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+
     static async Task Main(string[] args)
     {
+        Directory.CreateDirectory(logFolder);
+
         Console.WriteLine("Usage: IDScanNet.Authentication.SDK.Sample [folder]");
         Console.WriteLine();
 
-        var sw = Stopwatch.StartNew();
         Console.WriteLine("CreateService started");
-        using var service = await CreateService();
+        var sw = Stopwatch.StartNew();
+        using var service = await CreateServiceAsync();
         Console.WriteLine($"CreateService completed in {sw.ElapsedMilliseconds} ms");
 
-        var folders = args.Length != 0
-            ? args
-            : new string[] { Path.Combine("scanSets", "Failed") };
+        var folder = args.Length != 0 ? args[0] : Path.Combine("scanSets", "Failed");
 
-        foreach (var folder in folders)
-        {
-            await Authenticate(service, folder);
-        }
+        await AuthenticateAsync(service, folder);
 
         Console.WriteLine("Completed.");
         Console.WriteLine("Press \"Enter\" to close the sample application");
         Console.ReadLine();
     }
 
-    private static async Task<IAuthenticationService> CreateService(CancellationToken ct = default)
+    private static async Task<IAuthenticationService> CreateServiceAsync(CancellationToken ct = default)
     {
-        Directory.CreateDirectory("Authentication Logs");
-        //Simple authentication settings
+        // Simple authentication settings
         var authenticationServiceSettings = new AuthenticationServiceSettings
         {
-            //Set logging directory(default is c:\Users\Public\Documents\IDScan.net\Authentication.SDK\Logs\)
-            LoggingDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Authentication Logs"),
+            // Set logging directory(default is c:\Users\Public\Documents\IDScan.net\Authentication.SDK\Logs\)
+            LoggingDirectoryPath = logFolder,
 
-            //HostDataDirectoryPath - folder with data-files; can be substituted with a different value in case it's part of an app and data-files are somewhere close
-            HostDataDirectoryPath = @"C:\ProgramData\IDScan.net\IDScanNet.Authentication.SDK.Data",
-
-            //HostDirectoryPath = @""C:\Program Files (x86)\IDScan.net\IDScanNet.Authentication.SDK\Host\"
-
-            //Advanced authentication settings:
-            //Host - pipe name for connection. If it's not stated, then the default one will be used
-            //Port - ignore this for now, as it would be required sometime in the future
+            // HostDirectoryPath = @"C:\Program Files (x86)\IDScan.net\IDScanNet.Authentication.SDK\Host\"
         };
 
         var service = new AuthenticationService(authenticationServiceSettings);
 
-        //Fired when the document processing stage is changed
+        // Fired when the document processing stage has changed
         service.ProcessingStageChanged += (_, s) => Console.WriteLine($"Processing: {s.Status}");
 
-        //Fired when the error has occurred
-        service.ErrorReceived += (_, s) => Console.WriteLine("Error: {s.Text}");
+        // Fired when the error has occurred
+        service.ErrorReceived += (_, s) => Console.WriteLine($"Error: {s.Text}");
 
-        //Asynchronously initialize authentication service
+        // Initialize authentication service
         await service.InitializeAsync(ct);
+
+        Console.WriteLine($"HostVersion: {(await service.CheckAuthenticationHostAsync(ct)).HostVersion}");
 
         return service;
     }
 
-    private static async Task Authenticate(IAuthenticationService service, string folder, CancellationToken ct = default)
+    private static async Task AuthenticateAsync(IAuthenticationService service, string folder, CancellationToken ct = default)
     {
-        Console.WriteLine("----------------------------------------------------------------------------------------");
         Console.WriteLine($"Authenticate \"{folder}\":");
 
-        //Create a new authentication request
-        var request = new AuthenticationRequest();
-        request.Id = Guid.NewGuid();
-        request.Scan = CreateScanResult(folder);
+        // Create a new authentication request
+        var request = CreateAuthenticationRequest(folder);
 
-        //Asynchronously authenticate document
+        // Authenticate document
         var sw = Stopwatch.StartNew();
         var response = await service.ProcessAsync(request, ct);
-        Console.WriteLine($"Authentication Result for \"{folder}\" in : {sw.ElapsedMilliseconds} ms");
 
-        Console.WriteLine(ResponseToString(response));
+        var sb = new StringBuilder();
+        sb.AppendLine(DateTime.Now.ToString("o"));
+        sb.AppendLine($"Authentication Result for \"{folder}\" in : {sw.ElapsedMilliseconds} ms");
+        sb.AppendLine($"HostVersion: {(await service.CheckAuthenticationHostAsync()).HostVersion}");
+        ResponseToString(sb, response);
+        Console.WriteLine(sb.ToString());
+
+        File.AppendAllText(Path.Combine(logFolder, "SampleLog.txt"), sb.ToString());
     }
 
-    private static ScanResult CreateScanResult(string folder)
+    private static AuthenticationRequest CreateAuthenticationRequest(string folder)
     {
-        var result = new ScanResult();
+        var result = new AuthenticationRequest();
+        result.Scan = new ScanResult();
 
-        var documentDir = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folder));
-        foreach (var file in documentDir.EnumerateFiles())
-        {
-            if (string.Compare(file.Name, "Pdf417RawData.txt", true) == 0)
-            {
-                (result.RawItems ??= new()).Add(
-                    RawDataSource.PDF417,
-                    new RawData() { RawString = File.ReadAllText(file.FullName) });
+        var directory = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folder));
 
-                continue;
-            }
+        TryAddRawData(RawDataSource.PDF417, "Pdf417RawData");
 
-            ImageType? imageType = file.Name.Split('.')[0].ToLower() switch
-            {
-                "normal" => ImageType.ColorFront,
-                "normalback" => ImageType.ColorBack,
-                "uv" => ImageType.UVFront,
-                "uvback" => ImageType.UVBack,
-                "ir" => ImageType.IRFront,
-                "irback" => ImageType.IRBack,
-                "face" => ImageType.Face,
-                _ => null,
-            };
-
-            if (!imageType.HasValue ||
-                result.ScannedImages?.ContainsKey(imageType.Value) == true)
-            {
-                continue;
-            }
-
-            (result.ScannedImages ??= new()).Add(
-                imageType.Value,
-                File.ReadAllBytes(file.FullName));
-        }
+        TryAddImage(ImageType.ColorFront, "ScanNormalFront");
+        TryAddImage(ImageType.ColorBack, "ScanNormalBack");
+        TryAddImage(ImageType.UVFront, "ScanUvFront");
+        TryAddImage(ImageType.UVBack, "ScanUvBack");
+        TryAddImage(ImageType.IRFront, "ScanIrFront");
+        TryAddImage(ImageType.IRBack, "ScanIrBack");
+        TryAddImage(ImageType.Face, "Face");
 
         return result;
+
+        void TryAddRawData(RawDataSource rawDataSource, string filePattern)
+        {
+            var file = directory.EnumerateFiles($"*{filePattern}*.*").FirstOrDefault();
+            if (file != null)
+            {
+                result.Scan.RawItems ??= new();
+                result.Scan.RawItems.TryAdd(rawDataSource, new RawData() { RawString = File.ReadAllText(file.FullName) });
+            }
+        }
+
+        void TryAddImage(ImageType imageType, string filePattern)
+        {
+            var file = directory.EnumerateFiles($"*{filePattern}*.*").FirstOrDefault();
+            if (file != null)
+            {
+                result.Scan.ScannedImages ??= new();
+                result.Scan.ScannedImages.TryAdd(imageType, File.ReadAllBytes(file.FullName));
+            }
+        }
     }
 
-    private static string ResponseToString(AuthenticationResponse response)
+    private static void ResponseToString(StringBuilder sb, AuthenticationResponse response)
     {
         if (response.Result == null)
         {
-            return "Result unassigned!";
+            sb.AppendLine("Result unassigned!");
+            return;
         }
 
-        var sb = new StringBuilder();
-        sb
-            .AppendLine()
-            .AppendLine("Authentication status = " + response.Result?.AuthenticationStatus);
+        sb.AppendLine("Authentication status: " + response.Result.AuthenticationStatus);
 
-        sb
-            .AppendLine()
-            .AppendLine("Tests:");
+        sb.AppendLine("Tests:");
+        FormatAuthenticationTestResults(response.Result.GroupedResults);
 
-        var testGroups = response.Result.Results.GroupBy(a => a.TestGroup).ToList();
-        foreach (var testGroup in testGroups)
-        {
-            sb.AppendLine($"{testGroup.Key}:");
-            foreach (var testResult in testGroup)
-            {
-                sb.AppendLine($"    {testResult.Name} - {testResult.Type} {testResult.TestStatus} {testResult.Confidence}");
-
-                if (testResult.CrossMatches != null)
-                {
-                    foreach (var match in testResult.CrossMatches)
-                    {
-                        sb.AppendLine(
-                            $"          {match.FieldName} - {match.Item1.DataSourceString} = {match.Item1.Value};  {match.Item2.DataSourceString} = {match.Item2.Value} Confidence = {testResult.Confidence}");
-                    }
-                }
-            }
-        }
-
-        var jsonOpts = new JsonSerializerOptions()
+        var jsonOptions = new JsonSerializerOptions()
         {
             WriteIndented = true
         };
 
-        sb
-            .AppendLine()
-            .AppendLine("Document property value: ")
-            .AppendLine(JsonSerializer.Serialize(response.Document, jsonOpts));
+        sb.AppendLine();
+        sb.AppendLine("Document property value: ");
+        sb.AppendLine(JsonSerializer.Serialize(response.Document, jsonOptions));
 
-        sb
-            .AppendLine()
-            .AppendLine("PlainDocument property value: ")
-            .AppendLine(JsonSerializer.Serialize(response.PlainDocument, jsonOpts));
+        sb.AppendLine();
+        sb.AppendLine("PlainDocument property value: ");
+        sb.AppendLine(JsonSerializer.Serialize(response.PlainDocument, jsonOptions));
 
-        return sb.ToString();
+        void FormatAuthenticationTestResults(IEnumerable<AuthenticationTestResult> testResults, string indentStr = "  ")
+        {
+            foreach (var testResult in testResults ?? Enumerable.Empty<AuthenticationTestResult>())
+            {
+                sb.AppendLine($"{indentStr}{testResult.Name}: {testResult.Code} {testResult.TestStatus} {testResult.Confidence}");
+
+                foreach (var match in testResult.CrossMatches ?? Enumerable.Empty<CrossMatch>())
+                {
+                    sb.AppendLine(
+                        $"  {indentStr}Match \"{match.FieldName}\": {match.Item1.DataSourceString} = {match.Item1.Value}; {match.Item2.DataSourceString} = {match.Item2.Value}; Confidence = {match.Confidence}");
+                }
+
+                FormatAuthenticationTestResults(testResult.ChildTests, indentStr + "  ");
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # IDScanNet.Authentication.SDK.Sample
 
-This is a sample application for demonstrating how to use the Authentication SDK 
+This is a sample application for demonstrating how to use the IDScan Authentication SDK 
 
 ## Usage
 
-Execute TestApp without arguments to run ASDK on test scan sets.
+Execute Sample without arguments to run ASDK on test scan set.
 
-Execute TastApp with folder path as argument to run ASK on customer scan set.
+Execute Sample with folder path as argument to run ASK on customer scan set.
 
-TastApp can handle files in scan set:
-  Ir.*
-  IrBack.*
-  Normal.*
-  NormalBack.*
+Sample can handle files in scan set:
+```
   Pdf417RawData.txt
-  Uv.*
-  UvBack.*
 
+  *ScanIrBack*.*
+  *ScanIrFront*.*
+  *ScanNormalBack*.*
+  *ScanNormalFront*.*
+  *ScanUvBack*.*
+  *ScanUvFront*.*
+
+  *Face*.*
+```
